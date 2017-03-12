ae57d7ee5110e63e49281f94dc0616eaa89be6c3
malei <765362546@qq.com>
2017-03-12 21:34:33 +0800

    整理ch27的思路，详见 8/readme

---
+++ node.js学习3/8/ch27_1/settings.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/3/12.
+ */
+var host='127.0.0.1';
+var port=27017;
+var dbname="newcomment";
+
+module.exports={
+    db:'mongodb://'+host+':'+port+'/'+dbname
+}
