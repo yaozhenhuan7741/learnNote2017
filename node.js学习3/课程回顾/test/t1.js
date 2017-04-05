466ad997767cf3db0ac0679f59bd98809c27a0c0
malei <765362546@qq.com>
2017-04-05 21:57:12 +0800

    nodejs学习整理

---
+++ node.js学习3/课程回顾/test/t1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by malei on 2017/3/30.
+ */
+var a='123';
+console.log(a.charAt(1));
+console.log(a.valueOf());
+console.log(a.concat('ff'));
+
+var b=[1,2,3];
+console.log(b.valueOf());
+console.log(b);
